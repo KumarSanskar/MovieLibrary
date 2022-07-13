41b4b4c20b5f43cb25e52504f66d1d63d293c0c9
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2022-07-13 11:56:36 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+
+
+document.getElementById("loginForm").addEventListener("submit",(event)=>{
+    event.preventDefault()
+})
+
+firebase.auth().onAuthStateChanged((user)=>{
+    if(user){
+        location.replace("userprofile.html")
+    }
+})
+
+//this is login function used to authenticate data using firebase 
+function login(){
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .catch((error)=>{
+        document.getElementById("error").innerHTML = error.message
+    })
+}
+
+//this is signup function to create user using firebase
+function signUp(){
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .catch((error) => {
+        document.getElementById("error").innerHTML = error.message
+    });
+}
+
+//this is function to send email to user in case if he forgets password using firebase
+function forgotPass(){
+    const email = document.getElementById("email").value
+    firebase.auth().sendPasswordResetEmail(email)
+    .then(() => {
+        alert("Reset link sent to your email id")
+    })
+    .catch((error) => {
+        document.getElementById("error").innerHTML = error.message
+    });
+}
